a0ba8288060f9e73e9c79e0087d01c32bb7b358b
Leonardo Sales <leonardosales002@gmail.com>
2021-09-12 14:57:31 -0300

    BasicStaticComponents

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer(): JSX.Element {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/leobh31/projeto-sds3" target="_blank" rel="noreferrer">Leonardo Sales</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+
+    );
+}
+export default Footer;
